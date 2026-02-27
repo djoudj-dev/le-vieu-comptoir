af0d46137c1d8d62d33c38d280523c8e7897fd10
NÉDELLEC Julien <contact@nedellec-julien.fr>
2026-02-27 14:50:30 +0100

    commit first

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://djoudj-dev.github.io',
+  base: '/le-vieux-comptoir',
+  output: 'static',
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
