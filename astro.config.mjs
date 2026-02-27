bc15c46afd093382c8461d2e17b3e597b10ac30b
NÉDELLEC Julien <contact@nedellec-julien.fr>
2026-02-27 15:51:14 +0100

    fix: correct base url for github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://djoudj-dev.github.io',
-  base: '/le-vieux-comptoir',
+  base: '/le-vieu-comptoir',
   output: 'static',
   vite: {
     plugins: [tailwindcss()],
